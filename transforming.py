b21252458c5fb7d5267759b8d38f93a30b8bfc68
GizemGoksu <gizemgks_282@hotmail.com>
2024-08-28 02:06:55 +0300

    transform update

--- transforming.py
+++ transforming.py
@@ -1,4 +1,6 @@
 import functions
+from redlines import Redlines
+from IPython.display import display, Markdown, Latex, HTML, JSON
 
 # TRANSFORMING
 
@@ -26,6 +28,8 @@ and English pirate: \
 response = functions.get_completion(prompt)
 print(response)
 
+  # Formal and informal translation
+
 prompt = f"""
 Translate the following text to Spanish in both the \
 formal and informal forms: 
@@ -33,3 +37,98 @@ formal and informal forms:
 """
 response = functions.get_completion(prompt)
 print(response)
+
+# Universal Translator
+
+user_messages = [
+  "La performance du système est plus lente que d'habitude.",  # System performance is slower than normal         
+  "Mi monitor tiene píxeles que no se iluminan.",              # My monitor has pixels that are not lighting
+  "Il mio mouse non funziona",                                 # My mouse is not working
+  "Mój klawisz Ctrl jest zepsuty",                             # My keyboard has a broken control key
+  "我的屏幕在闪烁"                                               # My screen is flashing
+] 
+
+for issue in user_messages:
+    prompt = f"Tell me what language this is: ```{issue}```"
+    lang = functions.get_completion(prompt)
+    print(f"Original message ({lang}): {issue}")
+
+    prompt = f"""
+    Translate the following  text to English \
+    and Korean: ```{issue}```
+    """
+    response = functions.get_completion(prompt)
+    print(response, "\n")
+
+# Tone Translation
+
+prompt = f"""
+Translate the following from slang to a business letter: 
+'Dude, This is Joe, check out this spec on this standing lamp.'
+"""
+response = functions.get_completion(prompt)
+print(response)
+
+# Format Conversion
+
+data_json = { "resturant employees" :[ 
+    {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},
+    {"name":"Bob", "email":"bob32@gmail.com"},
+    {"name":"Jai", "email":"jai87@gmail.com"}
+]}
+
+prompt = f"""
+Translate the following python dictionary from JSON to an HTML \
+table with column headers and title: {data_json}
+"""
+response = functions.get_completion(prompt)
+print(response)
+
+# Spellcheck/Grammar check.
+
+text = [ 
+  "The girl with the black and white puppies have a ball.",  # The girl has a ball.
+  "Yolanda has her notebook.", # ok
+  "Its going to be a long day. Does the car need it’s oil changed?",  # Homonyms
+  "Their goes my freedom. There going to bring they’re suitcases.",  # Homonyms
+  "Your going to need you’re notebook.",  # Homonyms
+  "That medicine effects my ability to sleep. Have you heard of the butterfly affect?", # Homonyms
+  "This phrase is to cherck chatGPT for speling abilitty"  # spelling
+]
+
+# Proofread: Correcting Grammar and Spelling
+for t in text:
+    prompt = f"""Proofread and correct the following text
+    and rewrite the corrected version. If you don't find
+    and errors, just say "No errors found". Don't use 
+    any punctuation around the text:
+    ```{t}```"""
+    response = functions.get_completion(prompt)
+    print(response)
+
+text = f"""
+Got this for my daughter for her birthday cuz she keeps taking \
+mine from my room.  Yes, adults also like pandas too.  She takes \
+it everywhere with her, and it's super soft and cute.  One of the \
+ears is a bit lower than the other, and I don't think that was \
+designed to be asymmetrical. It's a bit small for what I paid for it \
+though. I think there might be other options that are bigger for \
+the same price.  It arrived a day earlier than expected, so I got \
+to play with it myself before I gave it to my daughter.
+"""
+# APA style
+prompt = f"""
+proofread and correct this review. Make it more compelling. 
+Ensure it follows APA style guide and targets an advanced reader. 
+Output in markdown format.
+Text: ```{text}```
+"""
+response = functions.get_completion(prompt)
+print(response)
+diff = Redlines(text,response) # Bunu kullanmanın amacı önceki ve sonraki hali arasındaki farkları görmek
+display(Markdown(diff.output_markdown))
+
+
+
+
+
