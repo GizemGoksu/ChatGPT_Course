f3378b555d4f1739b430fb52ba10a4b73bd0f649
GizemGoksu <gizemgks_282@hotmail.com>
2024-08-28 15:25:17 +0300

    chatbot update

--- Course_Content/chatbot.py
+++ Course_Content/chatbot.py
@@ -1,1 +1,32 @@
 import Function.main_functions as mf
+
+messages =  [  
+{'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
+{'role':'user', 'content':'tell me a joke'},   
+{'role':'assistant', 'content':'Why did the chicken cross the road'},   
+{'role':'user', 'content':'I don\'t know'}  ]
+
+response = mf.get_completion_from_messages(messages, temperature=1)
+print(response)
+
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},    
+{'role':'user', 'content':'Hi, my name is Isa'}  ]
+response = mf.get_completion_from_messages(messages, temperature=1)
+print(response)
+
+# Önceki promptlardaki response'larını hatırlayamadığı için burada kişisel bilgilerinizi bilmiyorum cevabı veriyor
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},    
+{'role':'user', 'content':'Yes,  can you remind me, What is my name?'}  ]
+response = mf.get_completion_from_messages(messages, temperature=1)
+print(response)
+
+messages =  [  
+{'role':'system', 'content':'You are friendly chatbot.'},
+{'role':'user', 'content':'Hi, my name is Isa'},
+{'role':'assistant', 'content': "Hi Isa! It's nice to meet you. \
+Is there anything I can help you with today?"},
+{'role':'user', 'content':'Yes, you can remind me, What is my name?'}  ]
+response = mf.get_completion_from_messages(messages, temperature=1)
+print(response)
